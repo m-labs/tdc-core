b954f9e1af8c5ba13ed16187482e1c4596b0110e
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-08-23 23:51:18 +0200

    manifest: update file list

--- core/Manifest.py
+++ core/Manifest.py
@@ -1,1 +1,5 @@
-files = ["tdc_delayline.vhd", "tdc_lbc.vhd"];
+files = [
+    "tdc_controller.vhd",  "tdc_freqc.vhd",     "tdc_psync.vhd",
+    "tdc_channelbank.vhd", "tdc_delayline.vhd", "tdc_lbc.vhd",     "tdc_ringosc.vhd",
+    "tdc_channel.vhd",     "tdc_divider.vhd",   "tdc_package.vhd", "tdc.vhd"
+];
