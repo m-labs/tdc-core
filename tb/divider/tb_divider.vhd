60aaf965932b64551c70cd1aeccd8fa972d477ff
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-09-19 12:03:01 +0200

    Update copyright notices

--- tb/divider/tb_divider.vhd
+++ tb/divider/tb_divider.vhd
@@ -15,7 +15,16 @@
 -- 2011-08-18 SB Created file
 -------------------------------------------------------------------------------
 
--- Copyright (C) 2011 Sebastien Bourdeauducq
+-- Copyright (C) 2011 CERN
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, version 3 of the License.
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -- DESCRIPTION:
 -- This test validates the integer divider by making it compute the quotient
