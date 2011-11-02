2cb843a5bbaf2e530faba662ce806f58be7a185e
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-11-02 20:44:46 +0100

    Revert "demo: add HPLL and serial_dac design files from White Rabbit r1344"
    This reverts commit 1a2cb36297e597e786c889d645a9cb541fdbe5cb.

--- demo/cores/hpll/build_wb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p doc
-wbgen2 -D ./doc/wrsw_helper_pll.html -V hpll_wb_slave.vhd -C ../../../software/include/hw/hpll_regs.h --cstyle defines --lang vhdl -K ../../sim/hpll_wb_regs.v hpll_wb.wb
--- demo/cores/hpll/manifest.py
+++
@@ -1,7 +0,0 @@
-files = [	"hpll_controller.vhd",
-					"wrsw_helper_pll.vhd",
-					"hpll_lock_detect.vhd",
-					"hpll_bangbang_pd.vhd",
-					"hpll_wb_slave.vhd",
-					"hpll_period_detect.vhd",
-					"wr_helper_pll.vhd"];
